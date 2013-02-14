f5d10e252ab8b1cf10431b4e38db24b50da171ee
Felipe <harnbb@gmail.com>
2013-02-13 22:46:37 -0200

    fix

--- FriendRecomendationExample.py
+++ FriendRecomendationExample.py
@@ -39,20 +39,20 @@ def friends_of_friends(graph,user,radius):
 def common_friends(graph, user1, user2):
     return friends(graph,user1).intersection(friends(graph,user2))
 
-def number_of_common_friends_map(graph, user):
+def _number_of_common_friends_map(graph, user):
     friends = friends_of_friends(graph,user,2)
     bigList = dict()
     for friend in friends:
         bigList[friend] = len(common_friends(graph,user,friend))
     return bigList
 
-def number_map_to_sorted_list(map):
+def _number_map_to_sorted_list(map):
 	return sorted(map.iteritems(), key=operator.itemgetter(1,0),reverse=True)
 
 def recommend_by_number_of_common_friends(graph, user,top):
-   return number_map_to_sorted_list(number_of_common_friends_map(graph,user))[:top]
+   return _number_map_to_sorted_list(_number_of_common_friends_map(graph,user))[:top]
 
-def influence_map(graph, user):
+def _influence_map(graph, user):
     friendsList = friends_of_friends(graph,user,2)
     bigList = dict()
     for friend in friendsList:
@@ -63,4 +63,4 @@ def influence_map(graph, user):
     return bigList
                                 
 def recommend_by_influence(graph, user,top):
-    return number_map_to_sorted_list(influence_map(graph,user))[:top]
+    return _number_map_to_sorted_list(_influence_map(graph,user))[:top]
